6370d1490cbc514f6555cbd145f9808e59bd9f70
larigoris <igorlarionov1033@gmail.com>
2025-10-14 16:39:14 +0300

    добавил модели в админ панель

--- studentpages/admin.py
+++ studentpages/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from studentpages.models import Student, Teacher, User, Taskanswer, Task, Daylist, Lesson, Portfolio, Studgroup, Subject
+
 # Register your models here.
+studentpages_models = [
+    admin.site.register(Student),
+    admin.site.register(Teacher),
+    admin.site.register(User),
+    admin.site.register(Taskanswer),
+    admin.site.register(Task),
+    admin.site.register(Daylist),
+    admin.site.register(Lesson),
+    admin.site.register(Portfolio),
+    admin.site.register(Studgroup),
+    admin.site.register(Subject),
+]
