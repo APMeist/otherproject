25bf8cdd42344da1f2187ec3e57a32f917bc3b94
SP <coloviev2@gmail.com>
2025-10-17 15:57:09 +0300

    Обновление
    Созданы формы для создания уроков через приложение и всякая другая прекрасная херня.
    
    Созданы небольшие бек вещи

--- studentpages/forms.py
+++ studentpages/forms.py
@@ -1,5 +1,5 @@
 from django.forms import ModelForm
-from models import portfolio
+from models import portfolio, lesson, task, taskanswer
 
 class StudentPortfolio(ModelForm):
     class Meta:
@@ -7,3 +7,25 @@ class StudentPortfolio(ModelForm):
         fields = ['name','fileportfolio','description']
 
 #пока хз зачем оно тут но Тоха сказал надо, я поверил :И
+# Оно нам надо для загрузки портфолио в БД :А
+
+# Создание урока - для админов возможно сделать отдельно модель в таблице тип просмотра
+class LessonForm(ModelForm):
+     class Meta:
+        model = lesson
+        fields = ['group', 'subject', 'teacher', 'name']
+        
+        
+ # Додумай сам       
+class TaskForm(ModelForm):
+    class Meta:
+        model = task
+        fields = ['title', 'description', 'deadline', 'task']
+        
+
+class TaskAnswerForm(ModelForm):
+    class Meta:
+             model = taskanswer
+             fields = ['answerer', 'task', 'filesanswer']
+             
+# по формам вроде все - вопрос теперь по рестрикшонам.
--- studentpages/views.py
+++ studentpages/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
-from .models import Portfolio
+
+from django.shortcuts import render, redirect
+
+from studentpages.models import task, taskanswer
+from .models import Portfolio, lesson
 from .models import Daylist
+from forms import LessonForm, TaskAnswerForm, TaskForm
 # Create your views here.
 
 def portfolio_page(request):
@@ -9,7 +13,69 @@ def portfolio_page(request):
     #хз какой писать путь, пускай пока так
     #не знаю в каком формате тебе лучше, я подумал,
     # так то по сути через шаблонизатор jinja все можно вывести в нужном уже формате
+    
+# Я с верхней хренью потом поиграюсь. Отображать будем на странице от 5 до 10 наверное файлов или просто 5, похер
 
 def daylist_page(request):
     daylist = Daylist.objects.all()
     return render(request, 'studentpages/studenthome.html', {'daylist': daylist})
+
+
+
+
+
+
+# Логин будет тут - ща похер на него
+
+
+
+# Создание урока
+
+def createLesson(request):
+    form = LessonForm()  
+    if request.method == 'POST': 
+            lesson.objects.create(
+            teacher=request.teacher,
+            name=request.POST.get('name'),
+            subject=request.POST.get('subject'),
+            description=request.POST.get('description'),
+        )
+            return redirect('lesson_page'),
+
+# Посмотри, что может быть еще сверху добавить, используй все, что хочешь. Я под таблами. Еще буду сидеть после тебя.    
+
+# Функция изменения урока - она может содержать в себе изменения. Нам надо посмотреть - какие изменения необходимо делать. А так - сделать колонку в БД как кабинет и все.
+#def updateLesson(request, pk):
+    #lesson = lesson.objects.get(id=pk)
+    #form = LessonForm(instance=lesson)
+    
+    
+    
+def createTask(request):
+    form = TaskForm()  
+    if request.method == 'POST': 
+            task.objects.create(
+                title=request.POST.get('title'),
+                description=request.POST.get('description'),
+                task=request.POST.get('task'),
+                deadline=request.POST.get('deadline'),
+            )
+            return redirect('task_page'),
+
+# Создание задания, тут потом выведем рестрикшн только лишь для учителя
+
+
+def createTaskAnswer(request):
+    form = TaskAnswerForm
+    if request.method == 'POST':
+            taskanswer.objects.create(
+                answerer=request.POST.get('answerer'),
+                task=request.POST.get('task'),
+                filesanswer=request.POST.get('filesanswer'),
+            )
+            return redirect('task_answer_page'),
+        
+# задание для наших маленьких людей.
+
+
+# Помилуй, Господи, сына твоего Антония. Да пусть код заработает так, как предпологается. Пусть прибудет воля твоя, прибудет царствие твое, прибудет благословение твое. Дай мне сил поставить на коленей недругов своих. Аминь.
