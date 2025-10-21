2de10d3a01881665e6ed60655567e90960de2b6e
SP <coloviev2@gmail.com>
2025-10-21 20:56:15 +0300

    1
    1

--- otherproject/urls.py
+++ otherproject/urls.py
@@ -21,7 +21,7 @@ from otherproject import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('mainsite.urls'))
+    path('', include('studentpages.urls'))
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
