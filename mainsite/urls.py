7240386eed60af1e52f46f669e603b6938bcad0b
larigoris <igorlarionov1033@gmail.com>
2025-10-21 15:04:27 +0300

    Изменил BinaryField на Filefield, Прикрутил хранение данных

--- mainsite/urls.py
+++ mainsite/urls.py
@@ -5,6 +5,5 @@ from django.contrib import admin
 urlpatterns = [
     path('', views.main_page, name="mainpage"),
     path('about/', views.about_page, name="aboutpage"),
-    path('admin/', admin.site.urls),
     path('student/', include('studentpages.urls')),
 ]
